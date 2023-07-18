9e41b585e324c1a3696feaf743fd39bb4f63c3b9
Chinmoy Thakuria <chinmoythakuria20@gmail.com>
2023-07-19 00:40:25 +0530

    frontend

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+import { Provider } from "react-redux";
+import { store } from "./store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "mern-backend-kappa.vercel.app"
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
